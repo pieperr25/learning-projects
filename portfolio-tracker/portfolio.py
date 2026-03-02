37374189010023b42ac0c6500018c45d60984db0
Ryan Pieper <pieperr25@gmail.com>
2026-03-02 17:23:23 -0600

    Add portfolio tracker project

---
+++ portfolio-tracker/portfolio.py
@@ -0,0 +1,42 @@
+import yfinance as yf
+import json
+print('Type "done" when finished')
+try:
+    with open('/Users/pieps/Projects/portfolio.json','r') as f:
+        portfolio = json.load(f)
+except FileNotFoundError:
+    portfolio = {}
+while True:
+    ticker = input('Ticker:')
+    if ticker == 'done':
+        break
+    while True:
+        try:
+            shares = int(input('Shares:'))
+            break
+        except ValueError:
+            print('Must be a valid integer')
+    try:
+        (yf.Ticker(ticker)).info['quoteType']
+        portfolio[ticker] = shares
+    except (KeyError, Exception):
+        print('That ticker does not exist')
+
+total_value = 0
+print(f"{'Stock':<16}{'Price':>10}{'Shares':>13}{'Value':>15}")
+print('-' * 54)
+for stock in portfolio:
+    ticker = yf.Ticker(stock)
+    info = ticker.info
+    price = info.get('currentPrice') or info.get('regularMarketPrice')
+    shares = portfolio[stock]
+    price_fmt = f'${price:.2f}'
+    value_fmt = f'${(price * shares):.2f}'
+    print(f'{stock.upper():<16}{price_fmt:>10}{str(shares):>13}{value_fmt:>15}')
+    total_value += price * shares
+
+print('-' * 54)
+formatted = f"${total_value:.2f}"
+print(f"{'Total':<39}{formatted:>15}")
+with open('/Users/pieps/Projects/portfolio.json', 'w') as f:
+    json.dump(portfolio, f)
